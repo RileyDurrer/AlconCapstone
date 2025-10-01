f2224f627c726d20d61bde92fa77a1c3a8dd8d3f
Riley Durrer <135176991+RileyDurrer@users.noreply.github.com>
2025-10-01 17:11:43 -0500

    started building prompt and API call

--- promptBuilder.py
+++ promptBuilder.py
@@ -1,3 +1,20 @@
 def buildPrompt(marketing_txt):
+    system_prompt = """
+        You are a compliance grader.
+        For each compliance policy, output:
+        - rule: the policy text
+        - pass: 2 if the text passes, 0 if it fails, 1 if a warning
+        - reason: short explanation
+
+        Return ONLY valid JSON in the format:
+        {
+        "results": [
+            {"rule": "...", "pass": 0 or 1, "reason": "..."},
+            {"rule": "...", "pass": 0 or 1, "reason": "..."}
+        ]
+        }
+        """
+    
+    
     prompt = f"Grade the following marketing material based on how it complies with the following policies and regulation \n\n{marketing_txt}\n\nProduct Description:"
     return prompt
